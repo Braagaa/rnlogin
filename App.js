56d428ce1627325356e6115ca2c6cfad999e3541
braagaa <michael_braga@outlook.com>
2021-03-24 12:08:54 -0400

    added env

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import React, { useState, useEffect } from "react";
-import RNLoginApi from "react-native-fido-login-api";
-import jwtDecode from "jwt-decode";
+import React, {useState, useEffect} from 'react';
+import RNLoginApi from 'react-native-fido-login-api';
+import jwtDecode from 'jwt-decode';
+import {API_KEY, BASE_URI} from '@env';
+import {testProps} from './utils/index';
 import {
   StyleSheet,
   View,
   Text,
   TextInput,
   TouchableOpacity,
-} from "react-native";
-
-const API_KEY =
-  "BFuCVG4A8a4lJLBAW_mTGHAQ9h1lGZKrG_GmVHY4UJYfCcPDdgbWKdeAtg0mTXbvd-34LhjfC2PCohG4WH-Lcg==";
-const BASE_URI = "https://729a5180-7bc2-11eb-b8e2-dfa0f6b67575.gw.v.loginid.io";
+} from 'react-native';
 
 const App = () => {
-  const [username, setUsername] = useState("");
+  const [username, setUsername] = useState('');
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const handleInput = (text) => {
@@ -22,11 +20,18 @@ const App = () => {
   };
 
   const handleLogin = async () => {
-    const res = await RNLoginApi.login(username);
+    let res;
+    if (username && !isLoggedIn) {
+      console.log('Logging with username');
+      res = await RNLoginApi.loginWithUsername(username);
+    } else {
+      console.log('Logging without username');
+      res = await RNLoginApi.login();
+    }
 
-    console.log(res);
     if (res.success) {
-      alert(`Successfully logged in!: ${username} ${res.jwt}`);
+      setIsLoggedIn(true);
+      alert(`Successfully logged in!: ${username}`);
       console.log(jwtDecode(res.jwt));
     } else {
       alert(`Failed to login: ${res.errorMessage}`);
@@ -37,7 +42,8 @@ const App = () => {
     const res = await RNLoginApi.registerWithUsername(username);
 
     if (res.success) {
-      alert(`Successfully registered!: ${username} ${res.jwt}`);
+      setIsLoggedIn(true);
+      alert(`Successfully registered!: ${username}`);
       console.log(jwtDecode(res.jwt));
     } else {
       alert(`Failed to register: ${res.errorMessage}`);
@@ -47,7 +53,7 @@ const App = () => {
   const handleLogout = () => {
     RNLoginApi.logout();
     setIsLoggedIn(false);
-    setUsername("");
+    setUsername('');
   };
 
   useEffect(() => {
@@ -64,7 +70,7 @@ const App = () => {
           alert(`Your account ${username} is not signed in.`);
         }
       } else {
-        alert("You do not have an account. Register an account.");
+        alert('You do not have an account. Register an account.');
       }
     };
     RNLoginApi.configure(API_KEY, BASE_URI);
@@ -81,19 +87,26 @@ const App = () => {
             style={styles.textInput}
             value={username}
             onChangeText={handleInput}
+            {...testProps()}
           />
         </View>
         <TouchableOpacity
           style={[styles.button, styles.marginTop]}
           onPress={handleLogin}
-        >
+          {...testProps()}>
           <Text style={styles.buttonText}>Login</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={handleRegister}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={handleRegister}
+          {...testProps()}>
           <Text style={styles.buttonText}>Register</Text>
         </TouchableOpacity>
         {isLoggedIn && (
-          <TouchableOpacity style={styles.button} onPress={handleLogout}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={handleLogout}
+            {...testProps()}>
             <Text style={styles.buttonText}>Logout</Text>
           </TouchableOpacity>
         )}
@@ -107,48 +120,48 @@ const styles = StyleSheet.create({
     flex: 1,
     width: null,
     height: null,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "snow",
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'snow',
   },
   textInput: {
     fontSize: 20,
-    fontWeight: "bold",
+    fontWeight: 'bold',
     height: 50,
     width: 100,
-    color: "snow",
+    color: 'snow',
     flex: 1,
     marginLeft: 20,
   },
   inputView: {
-    backgroundColor: "#7FC0CF",
+    backgroundColor: '#7FC0CF',
     borderRadius: 30,
-    width: "70%",
+    width: '70%',
     height: 45,
     marginBottom: 20,
-    alignItems: "center",
+    alignItems: 'center',
   },
   header: {
     fontSize: 20,
     marginBottom: 40,
-    color: "purple",
+    color: 'purple',
   },
   marginTop: {
     marginTop: 30,
   },
   button: {
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "steelblue",
-    width: "80%",
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'steelblue',
+    width: '80%',
     height: 50,
     borderRadius: 25,
     marginTop: 10,
   },
   buttonText: {
-    fontWeight: "bold",
+    fontWeight: 'bold',
     fontSize: 20,
-    color: "snow",
+    color: 'snow',
   },
   buttonDisabled: {
     opacity: 0.6,
